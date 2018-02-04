53032c0efc3b70a03a752b8aff2e0fa803e12148
IVO-LAPTOP\Ivo <ivo.anic@gmail.com>
2018-02-04 21:47:55 +0100

    clean code plus comments

--- petak.m
+++ petak.m
@@ -1,7 +1,6 @@
 clear all
 
 imgA = imread('bell_blured.png');
-imgB = imread('bell_blured_IG.png');
 A = imgA(:,:,1);
 
 imshow(imgA)
@@ -19,19 +18,18 @@ G = G/maxG;
 
 %kreæe moj dio
 Pround = round(P);
-D = zeros(11,11);
+D = zeros();
 dots = zeros ();
-dist = zeros ();
 V = zeros ();
 alfa = 0.01;
-
 edgeContainer = Pround;
-for iteracija = 1:5
+
+for iteracija = 1:1:3
     z = 1;
-for pos = 1:45
+for pos = 1:1:45 
     %koordinate toèke na kojoj se nalazim
-    x = Pround (1, pos);
-    y = Pround (2, pos);
+    x = edgeContainer (1, pos);
+    y = edgeContainer (2, pos);
     
     %tražim matricu 11x11 s centrom u ovoj toèki
     x1 = x-5;
@@ -39,9 +37,11 @@ for pos = 1:45
     y1 = y-5;
     y2 = y+5;
     Gtemp = G(x1:x2,y1:y2);
+    
+    %sluèaj za prvu toèku koju ne "radim"
     if pos-1 == 0
-        for i=1:11
-            for j=1:11
+        for i=1:1:11
+            for j=1:1:11
                 D(i,j) = 1 - Gtemp(i,j);
                 minBV = 0;
                 BcurIteration (i,j)= D(i,j) + minBV;
@@ -54,55 +54,62 @@ for pos = 1:45
         
     else
         %koordinate prethodne toèke
-        x0 = Pround (1, pos-1);
-        y0 = Pround (2, pos-1);
+        x0 = edgeContainer (1, pos-1);
+        y0 = edgeContainer (2, pos-1);
 
         %idem u petlju po elementima 11x11 matrice i odreðujem matricu B
-        for i=1:11
-            for j=1:11
-                for k=0:10
-                    for l=0:10
-                        %udaljenost izmeðu ove i prethodne toèke
+        for i=1:1:11
+            for j=1:1:11
+                for k=0:1:10
+                    for l=0:1:10
+                        %udaljenost izmeðu ove i prethodne toèke za svaku
+                        %toèku matrica
                         x00 = x0 - 5+k;
                         y00 = y0 - 5+l;
-                        x10 = x - 6+i; 
-                        y10 = y - 6+j;
+                        x10 = x - 5+i; 
+                        y10 = y - 5+j;
                         V (k+1,l+1) = alfa * ((x00-x10)^2+(y00-y10)^2);
                     end
-                end         
-               
-                [minBV, index] = min(min(BprevIteration + V));
-                Ttemp(i,j)=index;
-                D(i,j) = 1 - Gtemp(i,j);
+                end        
+                [minBV, index] = min(min(BprevIteration + V)); %dobio sam minBV i indeks na kojem se nalazi
+                Ttemp(i,j)=index; %spremam index min u matricu Ttemp
+                D(i,j) = 1 - Gtemp(i,j); %gmax-g
                 BcurIteration(i,j) = D(i,j) + minBV;
+                %spremam rezultat B u matricu dots
                 dots (i,j,z) = BcurIteration(i,j);
-                T (i,j,z) = Ttemp(i,j);
+                T (i,j,z) = Ttemp(i,j); %T ima indekse svih min
             end
         end
         BprevIteration = BcurIteration;
-        z = z + 1;
+        z = z + 1; %poveæaj count za sljedeæu toèku
     end
     
 end
 
-%X45 
+%odreðujem toèku X45' i nove koordinate 
 tempMatricaOva = dots (:,:,45);
 [min45,i45] = min(min(tempMatricaOva));
+%koordinate min X45 u matrici s vrijednstima B
 [x_min, y_min] = ind2sub(size(tempMatricaOva),i45);
 
+%X45(x0045,y0045) kako je zadano u p.txt
 x0045 = edgeContainer (1, 45);
 y0045 = edgeContainer (2, 45);
 xDiffLok45 = 5 - x_min;
 yDiffLok45 = 5 - y_min;
+
+%X45' (xGlob45, yGlob45)
 xGlob45 = x0045 - xDiffLok45;
 yGlob45 = y0045 - yDiffLok45;
 
+%spremanje u matricu s koordinatama novih toèaka (rubova)
 edgeContainer(1, 45)=(xGlob45);
 edgeContainer(2, 45)=(yGlob45);
     
 for lok = 44:-1:2
+    %petlja za odreðivanje svih ostalih toèaka
     tempT = T(:,:,lok+1);
-    index_neki = tempT(x_min,y_min);
+    index_neki = tempT(x_min,y_min); %index min vrijednosti dobivene u odnosu na prethodnu toèku
     
     tempMatricaOvdje = dots (:,:,lok);
     [x_min_temp, y_min_temp] = ind2sub(size(tempMatricaOvdje),index_neki);
@@ -120,4 +127,5 @@ for lok = 44:-1:2
     y_min = y_min_temp;
 end
 end
+%crtam graf s novim toèkama
 plot(edgeContainer(1,:),edgeContainer(2,:),'g')
